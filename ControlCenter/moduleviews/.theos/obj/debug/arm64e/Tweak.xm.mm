3521a81ad66d67e785fa5b05dcfe0a50868e100b
Vlad Munteanu <vladcmunteanu@gmail.com>
2020-01-07 00:06:58 -0500

    Tweaked basic cc module to have image as well.

--- ControlCenter/moduleviews/.theos/obj/debug/arm64e/Tweak.xm.mm
+++ ControlCenter/moduleviews/.theos/obj/debug/arm64e/Tweak.xm.mm
@@ -10,10 +10,14 @@
 }
 
 - (void)viewWillAppear:(BOOL)arg1;
+ 
+
+- (UIColor *)randomColor;
 @end
 
 @interface CCUIContentModuleContainerView : UIView
 @end
+
 
 
 #include <substrate.h>
@@ -37,14 +41,15 @@
 #endif
 
 @class CCUIModuleCollectionViewController; @class CCUIContentModuleContainerView; 
-static void (*_logos_orig$_ungrouped$CCUIModuleCollectionViewController$viewWillAppear$)(_LOGOS_SELF_TYPE_NORMAL CCUIModuleCollectionViewController* _LOGOS_SELF_CONST, SEL, BOOL); static void _logos_method$_ungrouped$CCUIModuleCollectionViewController$viewWillAppear$(_LOGOS_SELF_TYPE_NORMAL CCUIModuleCollectionViewController* _LOGOS_SELF_CONST, SEL, BOOL); 
+static void (*_logos_orig$_ungrouped$CCUIModuleCollectionViewController$viewDidAppear$)(_LOGOS_SELF_TYPE_NORMAL CCUIModuleCollectionViewController* _LOGOS_SELF_CONST, SEL, BOOL); static void _logos_method$_ungrouped$CCUIModuleCollectionViewController$viewDidAppear$(_LOGOS_SELF_TYPE_NORMAL CCUIModuleCollectionViewController* _LOGOS_SELF_CONST, SEL, BOOL); static void _logos_method$_ungrouped$CCUIModuleCollectionViewController$didPressButton$(_LOGOS_SELF_TYPE_NORMAL CCUIModuleCollectionViewController* _LOGOS_SELF_CONST, SEL, UIButton *); static UIColor * _logos_method$_ungrouped$CCUIModuleCollectionViewController$randomColor(_LOGOS_SELF_TYPE_NORMAL CCUIModuleCollectionViewController* _LOGOS_SELF_CONST, SEL); 
 static __inline__ __attribute__((always_inline)) __attribute__((unused)) Class _logos_static_class_lookup$CCUIContentModuleContainerView(void) { static Class _klass; if(!_klass) { _klass = objc_getClass("CCUIContentModuleContainerView"); } return _klass; }
-#line 17 "Tweak.xm"
+#line 21 "Tweak.xm"
+UIView *newView;
 
 
-static void _logos_method$_ungrouped$CCUIModuleCollectionViewController$viewWillAppear$(_LOGOS_SELF_TYPE_NORMAL CCUIModuleCollectionViewController* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, BOOL arg1) {
+static void _logos_method$_ungrouped$CCUIModuleCollectionViewController$viewDidAppear$(_LOGOS_SELF_TYPE_NORMAL CCUIModuleCollectionViewController* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, BOOL arg1) {
 
-    _logos_orig$_ungrouped$CCUIModuleCollectionViewController$viewWillAppear$(self, _cmd, arg1);
+    _logos_orig$_ungrouped$CCUIModuleCollectionViewController$viewDidAppear$(self, _cmd, arg1);
 
     NSLog(@"Hey Vlad Vlad!");
     NSDictionary *moduleDictionary = MSHookIvar<NSDictionary *>(self, "_moduleContainerViewByIdentifier");
@@ -52,12 +57,57 @@ static void _logos_method$_ungrouped$CCUIModuleCollectionViewController$viewWill
         id val = moduleDictionary[key];
         
         if([val isMemberOfClass:[_logos_static_class_lookup$CCUIContentModuleContainerView() class]]) {
-            [val setBackgroundColor:[UIColor redColor]];
+            [val setBackgroundColor:[self randomColor]];
         }
     }
+    
+    
+    
+    if (!newView) {
+        newView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 250, 250)];
+        [newView setBackgroundColor:[self randomColor]];
+    }
+    
+    
+    UIButton *newButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [newButton setTitle:@"Press Me!" forState: UIControlStateNormal];
+    [newButton setFrame:newView.frame];
+    
+    [newButton addTarget:self action:@selector(didPressButton:) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    
+    [newView addSubview:newButton];
+    [[[UIApplication sharedApplication] keyWindow] addSubview:newView];
+    
+    
+    UIImage *bobImage = [UIImage imageWithContentsOfFile:@"/Library/Application Support/ModuleViews.bundle/mia.png"];
+    UIImageView *bobImageView = [[UIImageView alloc] initWithImage:bobImage];
+    [bobImageView setFrame:CGRectMake(0, 250, 250, 250)];
+    
+    [[[UIApplication sharedApplication] keyWindow] addSubview:bobImageView];
+}
+
+
+static void _logos_method$_ungrouped$CCUIModuleCollectionViewController$didPressButton$(_LOGOS_SELF_TYPE_NORMAL CCUIModuleCollectionViewController* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, UIButton * arg1) {
+    [newView setBackgroundColor:[self randomColor]];
+    
+}
+
+
+static UIColor * _logos_method$_ungrouped$CCUIModuleCollectionViewController$randomColor(_LOGOS_SELF_TYPE_NORMAL CCUIModuleCollectionViewController* _LOGOS_SELF_CONST __unused self, SEL __unused _cmd) {
+    
+    int r = arc4random_uniform(256);
+    int g = arc4random_uniform(256);
+    int b = arc4random_uniform(256);
+    
+    
+    return [UIColor colorWithRed: r/255.0f green: g/255.0f blue: b/255.0f alpha:1.0f];
+    
+    
 }
 
 
 static __attribute__((constructor)) void _logosLocalInit() {
-{Class _logos_class$_ungrouped$CCUIModuleCollectionViewController = objc_getClass("CCUIModuleCollectionViewController"); MSHookMessageEx(_logos_class$_ungrouped$CCUIModuleCollectionViewController, @selector(viewWillAppear:), (IMP)&_logos_method$_ungrouped$CCUIModuleCollectionViewController$viewWillAppear$, (IMP*)&_logos_orig$_ungrouped$CCUIModuleCollectionViewController$viewWillAppear$);} }
-#line 35 "Tweak.xm"
+{Class _logos_class$_ungrouped$CCUIModuleCollectionViewController = objc_getClass("CCUIModuleCollectionViewController"); MSHookMessageEx(_logos_class$_ungrouped$CCUIModuleCollectionViewController, @selector(viewDidAppear:), (IMP)&_logos_method$_ungrouped$CCUIModuleCollectionViewController$viewDidAppear$, (IMP*)&_logos_orig$_ungrouped$CCUIModuleCollectionViewController$viewDidAppear$);{ char _typeEncoding[1024]; unsigned int i = 0; _typeEncoding[i] = 'v'; i += 1; _typeEncoding[i] = '@'; i += 1; _typeEncoding[i] = ':'; i += 1; memcpy(_typeEncoding + i, @encode(UIButton *), strlen(@encode(UIButton *))); i += strlen(@encode(UIButton *)); _typeEncoding[i] = '\0'; class_addMethod(_logos_class$_ungrouped$CCUIModuleCollectionViewController, @selector(didPressButton:), (IMP)&_logos_method$_ungrouped$CCUIModuleCollectionViewController$didPressButton$, _typeEncoding); }{ char _typeEncoding[1024]; unsigned int i = 0; memcpy(_typeEncoding + i, @encode(UIColor *), strlen(@encode(UIColor *))); i += strlen(@encode(UIColor *)); _typeEncoding[i] = '@'; i += 1; _typeEncoding[i] = ':'; i += 1; _typeEncoding[i] = '\0'; class_addMethod(_logos_class$_ungrouped$CCUIModuleCollectionViewController, @selector(randomColor), (IMP)&_logos_method$_ungrouped$CCUIModuleCollectionViewController$randomColor, _typeEncoding); }} }
+#line 85 "Tweak.xm"
--- ControlCenter/moduleviews/Tweak.xm
+++ ControlCenter/moduleviews/Tweak.xm
@@ -9,14 +9,19 @@
 }
 // Doesn't do anything, just showing it exists
 - (void)viewWillAppear:(BOOL)arg1;
+ 
+// %new
+- (UIColor *)randomColor;
 @end
 
 @interface CCUIContentModuleContainerView : UIView
 @end
 
+// Global Variables in Tweak
+UIView *newView;
 %hook CCUIModuleCollectionViewController
 
-- (void)viewWillAppear:(BOOL)arg1 {
+- (void)viewDidAppear:(BOOL)arg1 {
 
     %orig;
 
@@ -26,9 +31,54 @@
         id val = moduleDictionary[key];
         // checks if val is of type CCUIContentModuleContainerView
         if([val isMemberOfClass:[%c(CCUIContentModuleContainerView) class]]) {
-            [val setBackgroundColor:[UIColor redColor]];
+            [val setBackgroundColor:[self randomColor]];
         }
     }
+    
+    // New UIView
+    // This exists in memory but not on screen yet
+    if (!newView) {
+        newView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 250, 250)];
+        [newView setBackgroundColor:[self randomColor]];
+    }
+    
+    // New Button
+    UIButton *newButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [newButton setTitle:@"Press Me!" forState: UIControlStateNormal];
+    [newButton setFrame:newView.frame];
+    // Selector essentially
+    [newButton addTarget:self action:@selector(didPressButton:) forControlEvents:UIControlEventTouchUpInside];
+    
+    // Control Center acts more like a scroll view
+    // So we use this custom way
+    [newView addSubview:newButton];
+    [[[UIApplication sharedApplication] keyWindow] addSubview:newView];
+    
+    // Image
+    UIImage *bobImage = [UIImage imageWithContentsOfFile:@"/Library/Application Support/ModuleViews.bundle/mia.png"];
+    UIImageView *bobImageView = [[UIImageView alloc] initWithImage:bobImage];
+    [bobImageView setFrame:CGRectMake(0, 250, 250, 250)];
+    
+    [[[UIApplication sharedApplication] keyWindow] addSubview:bobImageView];
+}
+
+%new
+- (void)didPressButton:(UIButton *)arg1 {
+    [newView setBackgroundColor:[self randomColor]];
+    
+}
+
+%new
+- (UIColor *)randomColor {
+    // 0 - 255
+    int r = arc4random_uniform(256);
+    int g = arc4random_uniform(256);
+    int b = arc4random_uniform(256);
+    
+    // f implies float divison
+    return [UIColor colorWithRed: r/255.0f green: g/255.0f blue: b/255.0f alpha:1.0f];
+    
+    
 }
 
 %end
